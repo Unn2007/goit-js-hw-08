965959bc163bcac25eb9c606b2f98b9b8d29191c
Unn2007 <unn2007@i.ua>
2023-04-04 18:37:50 +0300

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+let seeTime = localStorage.getItem(STORAGE_KEY);
+if (seeTime) {
+    player.setCurrentTime(seeTime);
+};
+player.on('timeupdate', throttle(function({duration, percent, seconds}) {
+    localStorage.setItem(STORAGE_KEY, seconds);
+   
+},1000));
 
-player.on('play', function() {
-    console.log('played the video!');
-});
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
